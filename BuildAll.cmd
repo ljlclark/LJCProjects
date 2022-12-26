2e4ab8962bf614bb23e482bf362ea0068db6a5eb
ljlclark <35316538+ljlclark@users.noreply.github.com>
2022-12-27 07:37:25 +0800

    Doc Update

--- BuildAll.cmd
+++ BuildAll.cmd
@@ -1,3 +1,5 @@
+echo Copyright (c) Lester J. Clark and Contributors.
+echo Licensed under the MIT License.
 rem BuildAll.cmd
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
 
