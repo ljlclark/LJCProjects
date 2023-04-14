1d0b8bc0d63ec0c04d9cab9b6da33ace858f320d
ljlclark <35316538+ljlclark@users.noreply.github.com>
2023-04-14 10:02:33 +0800

    GenDoc Updates

---
+++ CoreUtilities/LJCDocLib/LJCDocLibDAL/bin/Debug/SQLScript/sp_ChangeSequence.sql
@@ -0,0 +1,102 @@
+﻿/* sp_ChangeSequence.sql */
+/* use [LJCData] */
+go
+set ansi_nulls on
+go
+set quoted_identifier on
+go
+
+if object_id ( 'dbo.sp_ChangeSequence', 'P' ) is not null
+  drop procedure dbo.sp_ChangeSequence;  
+go  
+create procedure dbo.sp_ChangeSequence
+  @table varchar(100),
+  @column varchar(100),
+  @sourceSequence int,
+  @targetSequence int,
+  @whereClause varchar(200)
+as
+begin
+/*
+  Add a sequence space: source = 0, target = space sequence.
+  Remove a sequence space: source = -1, target = space sequence.
+*/
+declare @endSequence int;
+declare @sql nvarchar(320);
+declare @params nvarchar(100);
+
+/* Get records end sequence. */
+set @sql = 'select @endSequence = max([' + @column + ']) + 2';
+set @sql += ' from ' + @table;
+set @sql += ' ' + @whereClause;
+set @params = '@endSequence int output';
+exec sp_executesql @sql, @params, @endSequence output;
+
+/* Move source Sequence to end sequence. */
+if (@sourceSequence > 0)
+begin
+  set @sql = 'update ' + @table;
+  set @sql += ' set [' + @column + '] = ' + cast(@endSequence as nvarchar(20));
+  set @sql += ' where [' + @column + '] = ' + cast(@sourceSequence as nvarchar(20));
+  if (@whereClause <> null)
+  begin
+    set @sql += ' and '+ @whereClause;
+  end
+  exec sp_executesql @sql;
+end
+else
+begin
+  if (0 = @sourceSequence)
+  begin
+    /* set to move everything from the target sequence down. */
+    set @sourceSequence = @endSequence;
+  end
+  else
+  begin
+    /* set to move everything from the target sequence up. */
+    set @sourceSequence = @targetSequence;
+	set @targetSequence = @endSequence;
+  end
+end
+
+if (@sourceSequence < @targetSequence)
+begin
+  /* Move contained sequences up to fill moved source space. */
+  set @sql = 'update ' + @table + ' set [' + @column + '] = ' + @column + ' - 1';
+  set @sql += ' where [' + @column + '] > ' + cast(@sourceSequence as nvarchar(20));
+  set @sql += ' and [' + @column + '] <= ' + cast(@targetSequence as nvarchar(20));
+  if (@whereClause <> null)
+  begin
+    set @sql += ' and '+ @whereClause;
+  end
+end
+else
+begin
+  if (@sourceSequence <> @targetSequence)
+  begin
+    /* Move contained sequences down to fill moved source space. */
+    set @sql = 'update ' + @table + ' set [' + @column + '] = ' + @column + ' + 1';
+    set @sql += ' where [' + @column + '] < ' + cast(@sourceSequence as nvarchar(20));
+    set @sql += ' and [' + @column + '] >= ' + cast(@targetSequence as nvarchar(20));
+    if (@whereClause <> null)
+    begin
+      set @sql += ' and '+ @whereClause;
+    end
+  end
+end
+exec sp_executesql @sql;
+
+/* Set new source sequence. */
+if (@sourceSequence <> @endSequence
+and @sourceSequence <> @targetSequence)
+begin
+  set @sql = 'update ' + @table + ' set [' + @column + '] = ' + cast(@targetSequence as nvarchar(20));
+  set @sql += ' where [' + @column + '] = ' + cast(@endSequence as nvarchar(20));
+  if (@whereClause <> null)
+  begin
+    set @sql += ' and '+ @whereClause;
+  end
+  exec sp_executesql @sql;
+end
+end
+go
---
+++ CoreUtilities/LJCDocLib/LJCDocLibDAL/bin/Debug/SQLScript/sp_ResetSequence.sql
@@ -0,0 +1,74 @@
+/* sp_ResetSequence.sql */
+/* use [LJCData] */
+go
+set ansi_nulls on
+go
+set quoted_identifier on
+go
+
+if object_id ( 'dbo.sp_ResetSequence', 'P' ) is not null
+  drop procedure dbo.sp_ResetSequence;  
+go  
+create procedure dbo.sp_ResetSequence
+  @table varchar(100),
+  @idColumn varchar(100),
+  @sequenceColumn varchar(100),
+  @where varchar(200)
+as
+begin
+declare @sql nvarchar(200);
+
+create table #Resequence (
+ ID int,
+ [Sequence] int
+);
+
+/* Copy records to temp table. */
+set @sql = 'insert into #Resequence'
+set @sql += '(ID, Sequence)';
+set @sql += ' select [' + @idColumn + '], [' + @sequenceColumn + ']';
+set @sql += ' from ' + @table;
+if (@where is not null)
+begin
+  set @sql += ' ' + @where;
+end
+exec sp_executesql @sql;
+
+/* Reset sequences in temp table. */
+declare @newSequence int = 0;
+declare @sequence int;
+declare @id int;
+
+declare varCursor cursor for
+select ID, [Sequence] 
+from #Resequence
+order by ID, [Sequence]; 
+
+open varCursor  
+fetch next from varCursor into @id, @sequence
+
+while @@FETCH_STATUS = 0  
+begin
+  set @newSequence = @newSequence + 1;
+  if (@sequence <> @newSequence)
+  begin
+    update #Resequence
+	set [Sequence] = @newSequence
+	where ID = @id;
+  end
+
+  fetch next from varCursor into @id, @sequence 
+end 
+
+close varCursor  
+deallocate varCursor
+
+/* Copy resequence values to original table. */
+set @sql = 'update t set';
+set @sql += ' t.[' + @sequenceColumn + '] = r.[Sequence]';
+set @sql += ' from ' + @table + ' t';
+set @sql += ' inner join #Resequence r'
+set @sql += ' on t.[' + @idColumn + '] = r.ID';
+exec sp_executesql @sql;
+end
+go
