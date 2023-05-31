f23c6621c258279644befe0545451808acadb336
ljlclark <35316538+ljlclark@users.noreply.github.com>
2023-06-01 06:43:27 +0800

    GenDoc Edit Updates

---
+++ CoreUtilities/LJCDocLib/LJCDocLibDAL/bin/Debug/SQLScript/sp_DocAssemblyShow.sql
@@ -0,0 +1,30 @@
+/* sp_DocAssemblyShow.sql */
+USE [LJCData]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF OBJECT_ID ( 'dbo.sp_DocAssemblyShow', 'P' ) IS NOT NULL   
+  DROP PROCEDURE dbo.sp_DocAssemblyShow;  
+GO  
+CREATE PROCEDURE dbo.sp_DocAssemblyShow
+  @assemblyID smallint
+AS
+BEGIN
+declare @assemblyGroupID smallint;
+
+select @assemblyGroupID=DocAssemblyGroupID
+from DocAssembly where ID = @assemblyID;
+
+select ID DocAssemblyGroup, Name, Heading, Sequence, ActiveFlag
+from DocAssemblyGroup where ID = @assemblyGroupID;
+select ID DocAssembly, DocAssemblyGroupID, Name, Description, Sequence
+ , FileSpec, MainImage, ActiveFlag
+from DocAssembly where ID = @assemblyID;
+select ID DocClass, DocAssemblyID, DocClassGroupID, Name, Description
+ , Sequence, ActiveFlag
+from DocClass where DocAssemblyID = @assemblyID;
+END
+GO
