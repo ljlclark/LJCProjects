b88693884c782f7a18fb276850b0770800519e7f
ljlclark <35316538+ljlclark@users.noreply.github.com>
2023-07-25 20:21:10 +0800

    Backup Updates

--- CoreUtilities/BackupWatcher/BackupLocal.cmd
+++ CoreUtilities/BackupWatcher/BackupLocal.cmd
@@ -5,18 +5,7 @@ echo Licensed under the MIT License.
 rem BackupLocal.cmd
 echo:
 
-del "C:\Users\Les\Documents\Visual Studio 2022\LJCProjectsDev\Build.txt"
-del "C:\Users\Les\Documents\Visual Studio 2022\LJCProjectsDev\Update.txt"
-del "C:\Users\Les\Documents\Visual Studio 2022\LJCProjectsDev\CoreUtilities\LJCCodeLineCounter\LJCCodeLineCounter\bin\Debug\ProjectFindLinesHTMLX.cmd "
-del "C:\Users\Les\Documents\Visual Studio 2022\LJCProjectsDev\CoreUtilities\LJCCodeLineCounter\LJCCodeLineCounter\bin\Debug\ProjectFindLinesX.cmd"
-del "C:\Users\Les\Documents\Visual Studio 2022\LJCProjectsDev\CoreUtilities\LJCCodeLineCounter\LJCCodeLineCounter\bin\Debug\ProjectFindLinesXMLX.cmd"
-del "C:\Users\Les\Documents\Visual Studio 2022\LJCProjectsDev\CoreUtilities\LJCCodeLineCounter\LJCCodeLineCounter\bin\Debug\CountLines.txt"
-del "C:\Users\Les\Documents\Visual Studio 2022\LJCProjectsDev\CoreUtilities\LJCCodeLineCounter\LJCCodeLineCounter\bin\Debug\FindLines.txt"
-del "C:\Users\Les\Documents\Visual Studio 2022\LJCProjectsDev\CoreUtilities\LJCCodeLineCounter\LJCCodeLineCounter\bin\Debug\LargeFiles.txt"
-del "C:\Users\Les\Documents\Visual Studio 2022\LJCProjectsDev\CoreUtilities\LJCCreateFileChanges\LJCCreateFileChanges\bin\Debug\FileChanges.txt"
-del "C:\Users\Les\Documents\Visual Studio 2022\LJCProjectsDev\CoreUtilities\LJCDocLib\LJCDocGen\bin\Debug\HTMLPageCount.txt"
-del "C:\Users\Les\Documents\Visual Studio 2022\LJCProjectsDev\CoreUtilities\LJCDocLib\LJCDocGen\bin\Debug\Missing.txt"
-del "C:\Users\Les\Documents\Visual Studio 2022\LJCProjectsDev\CoreUtilities\LJCGenDocEdit\LJCGenDocEdit\bin\Debug\ExportFiles\*.txt"
+call DeleteGenFiles.cmd
 
 set sourcePath="C:\Users\Les\Documents\Visual Studio 2022\LJCProjectsDev"
 set changeFile="bin\ChangeFile.txt"
@@ -27,16 +16,15 @@ set skipFiles="ChangeFile.txt|BuildAll.cmd|ClearBuild.cmd|UpdateAll.cmd"
 echo -----
 set targetPath="C:\Users\Les\Documents\Visual Studio 2022\LJCProjects_Stage"
 del %changeFile%
-echo bin\LJCCreateFileChanges %sourcePath% %targetPath% %changeFile% %multiFilter%
+echo bin\LJCCreateFileChanges %sourcePath% %targetPath% %changeFile% %multiFilter% %skipFiles%
 bin\LJCCreateFileChanges %sourcePath% %targetPath% %changeFile% %multiFilter% %skipFiles%
 echo bin\LJCBackupChanges %targetPath% %changeFile% %startFolder%
-bin\LJCBackupChanges %targetPath% %changeFile% %sourceFolder%
+bin\LJCBackupChanges %targetPath% %changeFile% %startFolder%
 
 echo -----
 set targetPath="C:\Users\Les\Documents\Visual Studio 2022\LJCProjects"
 del %changeFile%
-echo bin\LJCCreateFileChanges %sourcePath% %targetPath% %changeFile% %multiFilter%
+echo bin\LJCCreateFileChanges %sourcePath% %targetPath% %changeFile% %multiFilter% $skipFiles%
 bin\LJCCreateFileChanges %sourcePath% %targetPath% %changeFile% %multiFilter% %skipFiles%
 echo bin\LJCBackupChanges %targetPath% %changeFile% %startFolder%
-bin\LJCBackupChanges %targetPath% %changeFile% %sourceFolder%
-pause
+bin\LJCBackupChanges %targetPath% %changeFile% %startFolder%
--- CoreUtilities/BackupWatcher/BackupLocalBKP.cmd
+++ CoreUtilities/BackupWatcher/BackupLocalBKP.cmd
@@ -2,13 +2,21 @@ echo off
 echo:
 echo Copyright (c) Lester J. Clark and Contributors.
 echo Licensed under the MIT License.
-rem BackupLocal.cmd
+rem BackupLocalBKP.cmd
 echo:
 
+call DeleteGenFiles.cmd
+
+set sourcePath="C:\Users\Les\Documents\Visual Studio 2022\LJCProjectsDev"
 set changeFile="bin\ChangeFile.txt"
-set sourceFolder="LJCProjectsDev"
+set multiFilter="*.cs|*.cproj|*.sln|*.config|*.cmd|*.txt"
+set startFolder="LJCProjectsDev"
+set skipFiles="ChangeFile.txt|BuildAll.cmd|ClearBuild.cmd|UpdateAll.cmd"
 
 echo -----
 set targetPath="C:\Users\Les\Documents\Visual Studio 2022\LJCProjectsDevBKP"
-echo bin\LJCBackupChanges %targetPath% %changeFile% %sourceFolder%
-bin\LJCBackupChanges %targetPath% %changeFile% %sourceFolder%
+del %changeFile%
+echo bin\LJCCreateFileChanges %sourcePath% %targetPath% %changeFile% %multiFilter% %skipFiles%
+bin\LJCCreateFileChanges %sourcePath% %targetPath% %changeFile% %multiFilter% %skipFiles%
+echo bin\LJCBackupChanges %targetPath% %changeFile% %startFolder%
+bin\LJCBackupChanges %targetPath% %changeFile% %startFolder%
