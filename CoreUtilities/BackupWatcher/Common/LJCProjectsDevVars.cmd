f457541cfa9b84d86679e2bef74aa21580509f19
ljlclark <35316538+ljlclark@users.noreply.github.com>
2023-08-09 09:41:29 +0800

    Update LJCProjectsDevVars.cmd

--- CoreUtilities/BackupWatcher/Common/LJCProjectsDevVars.cmd
+++ CoreUtilities/BackupWatcher/Common/LJCProjectsDevVars.cmd
@@ -3,7 +3,7 @@ set sourcePath=%mainPath%\LJCProjectsDev"
 set changeFilePath=%mainPath%\LJCProjectsDev\CoreUtilities\BackupWatcher\CmdFiles
 set changeFileSpec=%changeFilePath%\ChangeFile.txt"
 set multiFilter="*.cs|*.csproj|*.sln|*.config|*.cmd|*.txt|Doc\*.xml"
-set skipFiles="ChangeFile.txt|PagesChangeFile.txt|BackupLog.txt|BuildAll.cmd|ClearBuild.cmd|UpdateAll.cmd"
+set skipFiles="ChangeFile.txt|PagesChangeFile.txt|BackupLog.txt|Build.txt|BuildAll.cmd|ClearBuild.cmd|UpdateAll.cmd"
 
 set startFolder="LJCProjectsDev"
 set binPath=%mainPath%\LJCProjectsDev\CoreUtilities\BackupWatcher\Bin
