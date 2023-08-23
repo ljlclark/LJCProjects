38004bf35c8c3b6bab2eb2b26ed41c5cbabb7f9f
ljlclark <35316538+ljlclark@users.noreply.github.com>
2023-08-23 08:57:10 +0800

    Update LJCProjectsBackup.cmd

--- CoreUtilities/BackupWatcher/CmdFiles/LJCProjectsBackup.cmd
+++ CoreUtilities/BackupWatcher/CmdFiles/LJCProjectsBackup.cmd
@@ -9,6 +9,17 @@ call ..\Common\DeleteGenFiles.cmd
 call ..\Common\LJCProjectsDevVars.cmd
 
 echo -----
+set targetPath=%mainPath%\LJCProjects_Stage"
+
+if exist %changeFileSpec% del %changeFileSpec%
+echo %binPath%\LJCCreateFileChanges" %sourcePath% %targetPath% %changeFileSpec% %multiFilter% %skipFiles%
+%binPath%\LJCCreateFileChanges" %sourcePath% %targetPath% %changeFileSpec% %multiFilter% %skipFiles%
+
+if exist %changeFilePath%\BackupLog.txt" del %changeFilePath%\BackupLog.txt
+echo %binPath%\LJCBackupChanges" %targetPath% %changeFileSpec% %startFolder%
+%binPath%\LJCBackupChanges" %targetPath% %changeFileSpec% %startFolder%
+
+echo -----
 set targetPath=%mainPath%\LJCProjects"
 
 if exist %changeFileSpec% del %changeFileSpec%
