d333d5e442430022d74ebe7b6853d00fb1d6c3b0
ljlclark <35316538+ljlclark@users.noreply.github.com>
2023-12-29 09:23:41 +0800

    DocGen CSS Updates

---
+++ CoreUtilities/LJCGenDoc/CSS/CodeDocCSSUpdate.cmd
@@ -0,0 +1,19 @@
+rem WebSitesDev
+copy CodeDoc.css ..\..\..\..\WebSitesDev\CodeDoc\LJCCodeDoc\CSS
+copy CodeDoc.css ..\..\..\..\WebSitesDev\CodeDoc\LJCDBDiagrams\CSS
+copy CodeDoc.css ..\..\..\..\WebSitesDev\CodeDoc\LJCPHPCodeDoc\CSS
+
+rem LJCProjectsDev
+copy CodeDoc.css ..\..\..\CSS
+
+rem CoreUtilities
+copy CodeDoc.css ..\GenDoc\bin\CSS
+copy CodeDoc.css ..\..\LJCViewEditor\CSS
+
+copy CodeDoc.css ..\..\..\CoreAssemblies\CSS
+copy CodeDoc.css ..\..\..\CoreAssemblies\LJCDataAccess\CSS
+copy CodeDoc.css ..\..\..\CoreAssemblies\LJCDataAccessConfig\CSS
+copy CodeDoc.css ..\..\..\CoreAssemblies\LJCDataDetail\CSS
+copy CodeDoc.css ..\..\..\CoreAssemblies\LJCDBClientLib\CSS
+copy CodeDoc.css ..\..\..\CoreAssemblies\LJCDBDataAccess\CSS
+copy CodeDoc.css ..\..\..\CoreAssemblies\LJCDBMessage\CSS
