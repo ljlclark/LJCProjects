c7dc5be32c2b9f0ffee2aec21f3cff6bfe6b97c8
ljlclark <35316538+ljlclark@users.noreply.github.com>
2023-12-29 09:08:22 +0800

    Delete CodeDocCSSUpdate.cmd

--- CSS/CodeDocCSSUpdate.cmd
+++
@@ -1,4 +0,0 @@
-copy CodeDoc.css ..\CoreUtilities\LJCGenText\LJCGenText\bin
-copy CodeDoc.css ..\CoreUtilities\LJCGenText\LJCGenText\bin\Debug
-copy CodeDoc.css ..\CoreUtilities\LJCDocLib\LJCDocGen\bin\Debug
-
