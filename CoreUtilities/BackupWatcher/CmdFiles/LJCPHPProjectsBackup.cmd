85ee0a17601e4a6c6f70d28f15d2f4c2e64f51ca
ljlclark <35316538+ljlclark@users.noreply.github.com>
2025-05-04 11:18:43 +0800

    Projects

--- CoreUtilities/BackupWatcher/CmdFiles/LJCPHPProjectsBackup.cmd
+++ CoreUtilities/BackupWatcher/CmdFiles/LJCPHPProjectsBackup.cmd
@@ -2,38 +2,26 @@ echo off
 echo:
 echo Copyright (c) Lester J. Clark and Contributors.
 echo Licensed under the MIT License.
-rem  LJCWebPagesBackup.cmd
+rem ***LJCPHPProjectsBackup.cmd
 echo:
 
-rem ** Set the code top-level path.
 set mainPath="C:\Users\Les\Documents\Visual Studio 2022
-
-rem ** Set the source (from) path.
-set sourcePath=%mainPath%\WebSitesDev\CodeDoc\LJCCodeDoc"
-
-rem ** Set the change file path and name.
+set sourcePath=%mainPath%\LJCPHPProjectsDev"
 set changeFilePath=%mainPath%\LJCProjectsDev\CoreUtilities\BackupWatcher\CmdFiles
 set changeFileSpec=%changeFilePath%\PagesChangeFile.txt"
-
-rem ** Set the files to copy and explicit skips.
-set multiFilter="*.html"
+set multiFilter="ReadMe*.txt|*.cmd|*.php"
 set skipFiles="index.html|CodeDoc.html"
 
-echo -----
-set targetFolder="LJCCodeDoc"
-set targetPath=%mainPath%\WebPages\%targetFolder%
-
-rem ** Delete the previous changes file PagesChangeFile.txt.
-if exist %changeFileSpec% del %changeFileSpec%
-
+set startFolder="GenDoc"
 set binPath=%mainPath%\LJCProjectsDev\CoreUtilities\BackupWatcher\Bin
-rem ** Create the new changes file PagesChangeFile.txt.
+
+echo -----
+set targetPath=%mainPath%\LJCPHPProjects\GenDoc"
+
+if exist %changeFileSpec% del %changeFileSpec%
 echo %binPath%\LJCCreateFileChanges" %sourcePath% %targetPath% %changeFileSpec% %multiFilter% %skipFiles%
 %binPath%\LJCCreateFileChanges" %sourcePath% %targetPath% %changeFileSpec% %multiFilter% %skipFiles%
 
-rem ** Delete the previous BackupLog.txt file.
 if exist %changeFilePath%\BackupLog.txt" del %changeFilePath%\BackupLog.txt
-
-rem ** Perform the backup using entries from the changes file PagesChangeFile.txt.
 echo %binPath%\LJCBackupChanges" %targetPath% %changeFileSpec% %startFolder%
 %binPath%\LJCBackupChanges" %targetPath% %changeFileSpec% %startFolder%
