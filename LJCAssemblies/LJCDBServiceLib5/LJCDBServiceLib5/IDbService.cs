62f49b2889bb8b4764fca94120674d351c46af84
ljlclark <35316538+ljlclark@users.noreply.github.com>
2026-04-26 06:27:21 +0800

    Net Common

---
+++ LJCAssemblies/LJCDBServiceLib5/LJCDBServiceLib5/IDbService.cs
@@ -0,0 +1,20 @@
+// Copyright(c) Lester J. Clark and Contributors.
+// Licensed under the MIT License.
+// IDBService.cs
+using System;
+using System.ServiceModel;
+
+namespace LJCDBServiceLib5
+{
+  // The Service Contract for performing database operations using request
+  // XML messages. 
+  /// <include path='items/IDbService/*' file='Doc/IDbService.xml'/>
+  [ServiceContract(Namespace = "ljspricket@gmail.com")]
+  public interface IDbService
+  {
+    // Executes the specified request XML message.
+    /// <include path='items/Execute/*' file='Doc/IDbService.xml'/>
+    [OperationContract]
+    string Execute(string request);
+  }
+}
