c29470e840276d82d6eed69a59360d1f3ae3ba93
ljlclark <35316538+ljlclark@users.noreply.github.com>
2026-05-10 04:47:47 +0800

    GenData2

---
+++ CoreUtilities/LJCGenDoc2/LJCGenDataGenLilb/LJCGenDataGen.cs
@@ -0,0 +1,77 @@
+﻿// Copyright (c) Lester J. Clark and Contributors.
+// Licensed under the MIT License.
+// LJCGenDataGen.cs
+using LJCDocDataDAL;
+using LJCNetCommon;
+using System.IO;
+
+namespace LJCGenDataGenLib
+{
+  // Contains methods to create GenData XML and HTML Doc from DocData XML.
+  /// <include path="members/LJCGenDataGen/*" file="Doc/LJCGenDataGen.xml"/>
+  public class LJCGenDataGen
+  {
+    #region Constructor Methods
+
+    /// <summary>
+    /// Initializes an object instance.
+    /// </summary>
+    public LJCGenDataGen()
+    {
+    }
+
+    // Sets the GenCodeDoc config.
+    /// <include path="members/SetConfig/*" file="Doc/LJCGenDataGen.xml"/>
+    public void SetConfig(LJCGenDocConfig config)
+    {
+      HTMLPath = "CodeDoc/HTML";
+      GenDocConfig = config;
+      if (NetString.HasValue(config.OutputPath))
+      {
+        HTMLPath = config.OutputPath;
+      }
+      Directory.CreateDirectory(HTMLPath);
+    }
+    #endregion
+
+    #region Methods
+
+    public string SerializeGenData(string docDataXML, string codeFileSpec)
+    {
+      string retGenDataXML = null;
+
+      DocDataFile = NetCommon.XmlDeserializeMessage(typeof(LJCDocDataFile)
+        , docDataXML) as LJCDocDataFile;
+
+      return retGenDataXML;
+    }
+
+    public void GenTypePages()
+    {
+      foreach (LJCDocDataClass docDataClass in DocDataFile.Classes)
+      {
+        var genType = new LJCGenType(GenDocConfig, docDataClass);
+        
+      }
+    }
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Gets or sets the DocData.
+    /// </summary>
+    public LJCDocDataFile DocDataFile { get; set; }
+
+    /// <summary>
+    /// Gets or sets the GenDoc configuration.
+    /// </summary>
+    public LJCGenDocConfig GenDocConfig { get; set; }
+
+    /// <summary>
+    /// Gets or sets the path for HTML output.
+    /// </summary>
+    public string HTMLPath { get; set; }
+    #endregion
+  }
+}
---
+++ CoreUtilities/LJCGenDoc2/LJCGenDataGenLilb/LJCGenType.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Lester J. Clark and Contributors.
+// Licensed under the MIT License.
+// LJCGenType.cs
+using LJCDocDataDAL;
+using LJCGenTextLib;
+using System.IO;
+
+namespace LJCGenDataGenLib
+{
+  public class LJCGenType
+  {
+    public LJCGenType(LJCGenDocConfig config, LJCDocDataClass docDataClass)
+    {
+      GenDocConfig = config;
+      HTMLFolderName = Path.Combine(config.OutputPath
+        , $@"HTML\{docDataClass.Name}");
+      HTMLFileName = $"{docDataClass.Name}.html";
+      HTMLFileSpec = Path.Combine(HTMLFolderName, HTMLFileName);
+    }
+
+    public Sections GenDataClass(LJCDocDataClass docDataClass)
+    {
+      var retSections = new Sections();
+
+      var mainSection = retSections.Add("Main");
+      var repeatItem = mainSection.RepeatItems.Add("Main");
+      var mainReplacements = repeatItem.Replacements;
+      //mainReplacements.Add("_AssemblyName_", DataAssembly.Name);
+      //mainReplacements.Add("_AssemblyHtm_", GenAssembly.HTMLFileName);
+      var copyRight = "Copyright &copy Lester J. Clark and Contributors.<br />\r\n";
+      copyRight += "Licensed under the MIT License.";
+      mainReplacements.Add("_Copyright_", copyRight);
+      return retSections;
+    }
+
+    /// <summary>
+    /// Gets or sets the GenDoc configuration.
+    /// </summary>
+    public LJCGenDocConfig GenDocConfig { get; set; }
+
+    string HTMLFolderName { get; set; }
+
+    string HTMLFileName { get; set; }
+
+    string HTMLFileSpec { get; set; }
+  }
+}
